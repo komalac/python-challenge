c55c2472f42f75a59b6068aaca3b70961a0c6556
Komala <komala.chandran@capgemini.com>
2018-09-16 01:34:30 -0400

    Added Comments

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -1,21 +1,28 @@
 import os
 import csv
 
+#Declaring all lists
 DTList= []
 PLList = []
 TotPL = 0
 PLChgList = []
 
+#Opening CSV file
 filepath = os.path.join("Resources", "budget_data.csv")
 with open(filepath, newline="") as BdgDataFile:
     
     ReadBdgData = csv.reader(BdgDataFile)    
+
+    #writing data into a list
     for row in ReadBdgData:
         DTList.append(row[0])
         PLList.append(row[1])
 
+    #deleting header row
     del PLList[0]
     del DTList[0]
+
+    #getting the first row value to skip it in loop, for purpose of change in PL calcualtion
     PrevChg = int(PLList[0])
     PLChg = 0
     TotChg = 0
@@ -23,6 +30,7 @@ with open(filepath, newline="") as BdgDataFile:
 
     LLength = len(PLList)
     
+    #looping in to calculate
     for pl in range(LLength):
         TotPL = TotPL + int(PLList[pl])  
         PLChg = int(PLList[pl]) - PrevChg
@@ -36,14 +44,17 @@ with open(filepath, newline="") as BdgDataFile:
     maxPLDate = PLChgList.index(maxPL)
     minPL = min(PLChgList) 
     minPLDate = PLChgList.index(minPL)
-    print("Financial Analysis")
-    print("----------------------------")
-    print(f"Total Months: {len(PLList)}")
-    print(f"Total: {TotPL}")
-    print(f"Average  Change: ${round(TotChg/LLength, 2)}")
-    print(f"Greatest Increase in Profits: {DTList[maxPLDate]} (${maxPL})")
-    print(f"Greatest Decrease in Profits: {DTList[minPLDate]} (${minPL})")
 
+#printing on the terminal
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {len(PLList)}")
+print(f"Total: {TotPL}")
+print(f"Average  Change: ${round(TotChg/LLength, 2)}")
+print(f"Greatest Increase in Profits: {DTList[maxPLDate]} (${maxPL})")
+print(f"Greatest Decrease in Profits: {DTList[minPLDate]} (${minPL})")
+
+#writing into csv
 OPfile = open("output.txt", "w")
 OPfile.write("Financial Analysis\n")
 OPfile.write("----------------------------\n")
--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -1,48 +1,57 @@
 import os
 import csv
 
+#Declaring all lists
 VIDList= []
 CountyList = []
 CandidateList = []
 VoteCount = []
 
+#Opening csv file
 filepath = os.path.join("Resources", "election_data.csv")
 with open(filepath, newline="") as ElecDataFile:
     ReadElecData = csv.reader(ElecDataFile)    
-
+    
+    #creating list for each column
     for row in ReadElecData:
         VIDList.append(row[0])
         CountyList.append(row[1])
         CandidateList.append(row[2])
 
+    #deleting header row
     del VIDList[0]
     del CountyList[0]
     del CandidateList[0]
 
     TotVote = len(VIDList)
     
+    #printing in terminal
     print("Election Results")
     print("-------------------------")
     print(f"Total Votes: {TotVote}")
     print("-------------------------")
     
-
+    #Opening the output file
     OPfile = open("output.txt", "w")
+
+    #writing into the output file
     OPfile.write("Election Results\n")
     OPfile.write("-------------------------\n")
     OPfile.write(f"Total Votes: {TotVote}\n")
     OPfile.write("-------------------------\n")
 
-
+    #Unique Candidate names
     CanSet = set(CandidateList)
     UniCandidateList = list(CanSet)
-    CLLength = len(UniCandidateList)
-    UCLLength = len(CandidateList)
 
-    for ucl in range(CLLength):
+    UCLLength = len(UniCandidateList)
+    CLLength = len(CandidateList)
+
+    #looping through the unique lists, nested with the main candidate list
+    for ucl in range(UCLLength):
         CanTotVote = 0
         TotNumVote = 0
-        for cl in range(UCLLength):
+        for cl in range(CLLength):
             if CandidateList[cl] == UniCandidateList[ucl]:
                 CanTotVote = CanTotVote + 1
 
@@ -60,6 +69,3 @@ with open(filepath, newline="") as ElecDataFile:
     OPfile.write("Winner: " + UniCandidateList[WinnerCnd])
     OPfile.write("\n-------------------------\n")
   
-  
- 
-        
